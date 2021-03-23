ae8b782494ff43abe8ce171a27c18b0e19e55e36
Jorge Apodaca <mcsd@jorge-apodaca.net>
2021-03-23 16:28:41 -0700

    more content

--- python/boolean-expressions.py
+++ python/boolean-expressions.py
@@ -1,4 +1,38 @@
 #!/bin/python3
 
-# Boolean Expressions
+# Boolean Expressions (True or False)
 
+bool1 = True
+bool2 = 3*3 == 9
+bool3 = False
+bool4 = 3*3 != 9
+
+print(bool1, bool2, bool3, bool4)
+print(type(bool1))
+
+bool5 = "True"
+
+print(type(bool5))
+
+# Relational and Boolean Operators
+
+print("Relational and Boolean Operators:")
+
+greater_than = 7 > 5
+less_than = 5 < 7
+greater_than_equal_to = 7 >= 7
+less_than_equal_to = 7 <= 7
+
+print(greater_than, less_than, greater_than_equal_to, less_than_equal_to)
+
+# Multiple operators
+test_and = (7 > 5) and (5 < 7) #True
+test_and2 = (7 > 5) and (5 > 7) #False
+test_or = (7 > 5) or (5 < 7) #True
+test_or2 = (7 > 5) or (5 > 7) #True
+
+print(test_and, test_and2, test_or, test_or2)
+
+test_not = not True #False
+
+print(test_not)
