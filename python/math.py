8297f1b4f753f211863533c6c8204cc3273d07f2
Jorge Apodaca <mcsd@jorge-apodaca.net>
2021-03-23 15:09:57 -0700

    math python course

---
+++ python/math.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+# Math
+
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 /50) #PEMDAS
+print(50 ** 2) #exponents
+print(50 % 6) #modulo
+print(50 / 6) #divide
+print(50 // 6) #no leftovers
