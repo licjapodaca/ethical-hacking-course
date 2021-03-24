84f3d52b22086f0716448a0a79391082623bca7b
Jorge Apodaca <mcsd@jorge-apodaca.net>
2021-03-23 23:30:36 -0700

    minor changes

--- python/tuples.py
+++ python/tuples.py
@@ -4,4 +4,4 @@
 
 grades = ("a", "b", "c", "d", "e", "f")
 
-print(grades[1:]) # same capabilities like lists to get the content
+print(grades[1:]) # same capabilities like lists to get the content.
